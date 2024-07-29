80a68648606a19287bc1da895abdb4bac0b32750
pauvel <paulsystem32@gmail.com>
2024-07-28 21:33:50 -0700

    Initial Commit

---
+++ learning/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learning
+//
+//  Created by Paul Veliz on 28/07/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
